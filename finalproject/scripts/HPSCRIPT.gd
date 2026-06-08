186e73f9146a221887fb39685834c8f33e1f1574
jusMarz <jonas.aj.08@gmail.com>
2026-06-08 14:01:38 -0400

    Add files via upload
    useless

---
+++ finalproject/scripts/HPSCRIPT.gd
@@ -0,0 +1,7 @@
+extends Sprite2D
+var sprites = ["res://assets/0HP.png", "res://assets/10HP.png", "res://assets/20HP.png", "res://assets/30HP.png", "res://assets/40HP.png", "res://assets/50HP.png", "res://assets/60HP.png", "res://assets/70HP.png", "res://assets/80HP.png", "res://assets/90HP.png", "res://assets/100HP.png"]
+
+#func _ready() -> void:
+	#self.texture = sprites[0]
+	#
+		
